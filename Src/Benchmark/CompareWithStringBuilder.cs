ad5d47d0997c8e9a8f9e1b2daa3d1ae14e389069
Vyacheslav Brevnov <softstonedevelop@gmail.com>
2026-04-07 01:02:35 +0300

    feat: + Bump version .NET

--- Src/Benchmark/CompareWithStringBuilder.cs
+++ Src/Benchmark/CompareWithStringBuilder.cs
@@ -1,15 +1,17 @@
 ﻿using BenchmarkDotNet.Attributes;
 using BenchmarkDotNet.Jobs;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace Benchmark
 {
     [MemoryDiagnoser]
-    [SimpleJob(RuntimeMoniker.Net70)]
+    [SimpleJob(RuntimeMoniker.Net10_0)]
+    [SimpleJob(RuntimeMoniker.NativeAot10_0)]
     [HideColumns("Error", "StdDev", "Median", "RatioSD", "Gen0", "Gen1", "Gen2")]
     public partial class CompareWithStringBuilder
     {
-        [Params(5, 7, 10, 50, 100, 1000, 2500, 5_000, 10_000, 100_000, 500_000, 1_071_741)]
+        [Params(5, 25, 100, 1000)] // 1071741 is max
         public int StrLength;
 
         [Benchmark(Baseline = true, Description = "StringBuilder")]
@@ -24,7 +26,29 @@ namespace Benchmark
             var result = stringBuilder.ToString();
         }
 
-        [Benchmark(Description = "Class.StringCreator")]
+        [Benchmark(Description = "DefaultInterpolatedStringHandler")]
+        public void DefaultInterpolatedStringHandler()
+        {
+            var builder = new DefaultInterpolatedStringHandler();
+            for (int i = 0; i < StrLength; i++)
+            {
+                builder.AppendLiteral($"{i % 10}");
+            }
+
+            var result = builder.ToStringAndClear();
+        }
+
+        [Benchmark(Description = "Concatenation")]
+        public void Concatenation()
+        {
+            string result = string.Empty;
+            for (int i = 0; i < StrLength; i++)
+            {
+                result += $"{i % 10}";
+            }
+        }
+
+        [Benchmark(Description = "StringCreator")]
         public void ClassStringCreator()
         {
             var stringCreator = new StringCreator.Class.StringCreator(StrLength);
